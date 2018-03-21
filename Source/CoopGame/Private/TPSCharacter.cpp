2bb1a0648121c9a2817668808d48d10beabcd3f6
AlexanderKasimov <strelokstalk@yandex.ru>
2018-03-21 22:49:35 +0500

    Initial commit

---
+++ Source/CoopGame/Private/TPSCharacter.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TPSCharacter.h"
+
+
+// Sets default values
+ATPSCharacter::ATPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void ATPSCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector() * Value);
+}
+
+
+void ATPSCharacter::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector() * Value);
+}
+
+// Called every frame
+void ATPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ATPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ATPSCharacter::MoveRight);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &ATPSCharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookRight", this, &ATPSCharacter::AddControllerYawInput);
+
+
+}
+
